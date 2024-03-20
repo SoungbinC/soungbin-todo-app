d2cf50889a8d3d525b1bfc48ae22dc473c404462
Soungbin Cho <soungbinc@gmail.com>
2024-03-20 03:05:03 -0700

    publishing

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -15,6 +15,7 @@ function ToDoList() {
         <div>
             <h1>To Dos</h1>
             <hr />
+
             <select value={category} onInput={onInput}>
                 <option value={Categories.TO_DO}>To Do</option>
                 <option value={Categories.DOING}>Doing</option>
